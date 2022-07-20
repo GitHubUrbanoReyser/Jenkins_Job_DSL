f399aba3b4a858c8572eecdca33fad3d1cbededc
GitHubUrbanoReyser <106035896+GitHubUrbanoReyser@users.noreply.github.com>
2022-07-20 18:53:34 -0500

    update code groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,13 +1,13 @@
 job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
+	description('Job DSL de ejemplo')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/GitHubUrbanoReyser/Jenkins_Job_DSL.git', 'main') { node ->
+        		node / gitConfigName('demo')
+        		node / gitConfigEmail('demo@gmail.com')
       		}
     	} 
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+   		stringParam('nombre', defaultValue = 'Jenkins', description = 'Parametro de cadena para el Job Booleano')
       		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
       		booleanParam('agente', false)
     	}
@@ -18,7 +18,7 @@ job('ejemplo2-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
+      		mailer('demo@gmail.com', true, true)
       		slackNotifier {
 		  notifyAborted(true)
 		  notifyEveryFailure(true)
